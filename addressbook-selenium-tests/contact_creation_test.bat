53320c72dc19793e2fa350a339c66bc438fccfae
spkrylova <svetlana.krylova@billing.ru>
2015-09-22 19:21:05 +0300

    task 13 completed
    task 13 completed

---
+++ addressbook-selenium-tests/contact_creation_test.bat
@@ -0,0 +1,1 @@
+java -cp bin;C:\Users\skrylova\Downloads\xstream-1.4.8\lib\xstream-1.4.8.jar;C:\Users\skrylova\Downloads\xstream-1.4.8\lib\xstream\xpp3_min-1.1.4c.jar;C:\Users\skrylova\Downloads\xstream-1.4.8\lib\xstream\xmlpull-1.1.3.1.jar;C:\Users\skrylova\Downloads\selenium-server-standalone-2.47.1.jar org.testng.TestNG testng-customsuite.xml 
